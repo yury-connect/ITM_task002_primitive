b2c1c51de518c2274b416eb7e716d51c5f1e65cf
Yury <yury.connect@gmail.com>
2024-08-26 23:33:29 +0300

    Tasks: task0437

--- src/main/java/tech/it_mentor/part03_GettingToKnow_Branches_Cycles/task0437/Solution.java
+++ src/main/java/tech/it_mentor/part03_GettingToKnow_Branches_Cycles/task0437/Solution.java
@@ -26,7 +26,11 @@ Requirements:
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
     }
 }
