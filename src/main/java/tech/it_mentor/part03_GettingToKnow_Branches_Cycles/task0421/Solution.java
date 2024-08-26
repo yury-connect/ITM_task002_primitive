06e24217450cefea573c07dc635b3ae46d3818fe
Yury <yury.connect@gmail.com>
2024-08-26 23:12:35 +0300

    Tasks: task0421

--- src/main/java/tech/it_mentor/part03_GettingToKnow_Branches_Cycles/task0421/Solution.java
+++ src/main/java/tech/it_mentor/part03_GettingToKnow_Branches_Cycles/task0421/Solution.java
@@ -2,6 +2,7 @@ package tech.it_mentor.part03_GettingToKnow_Branches_Cycles.task0421;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* 
 Настя или Настя?
@@ -19,7 +20,14 @@ Requirements:
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String name1 = scanner.next().trim();
+        String name2 = scanner.next().trim();
 
+        if (name1.equals(name2)) {
+            System.out.println("Имена идентичны");
+        } else if (name1.length() == name2.length()) {
+            System.out.println("Длины имен равны");
+        }
     }
 }
